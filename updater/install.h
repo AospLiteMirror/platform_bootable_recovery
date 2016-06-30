fe45db0d349f4c76849b95b5a7c9e054ed55a992
Elliott Hughes <enh@google.com>
2016-06-30 09:28:42 -0700

    Fix install.h's use of attribute printf.
    And move off the bionic __nonnull macro, which I'm removing.
    
    Change-Id: I40b4424f4fd7bd8076e0eee3ec35de36c3ded8de

--- updater/install.h
+++ updater/install.h
@@ -19,9 +19,12 @@
 
 void RegisterInstallFunctions();
 
+//  __attribute__((format(printf, a, b)))这种写法
+// __attribute__ format属性可以给被声明的函数加上类似printf或者scanf的特征，它可以使编译器检查函数声明和函数实际调用参数之间的格式化字符串是否匹配。format属性告诉编译器，按照printf, scanf等标准C函数参数格式规则对该函数的参数进行检查
+// format的语法格式为：format (archetype, string-index, first-to-check) “archetype”指定是哪种风格；“string-index”指定传入函数的第几个参数是格式化字符串；“first-to-check”指定从函数的第几个参数开始按上述规则进行检查。
 // uiPrintf function prints msg to screen as well as logs
-void uiPrintf(State* state, const char* format, ...);
+void uiPrintf(State* _Nonnull state, const char* _Nonnull format, ...) __attribute__((__format__(printf, 2, 3)));
 
-static int make_parents(char* name);
+static int make_parents(char* _Nonnull name);
 
 #endif
