091f336495eb41238f8a48930a97b26fa885aec3
codelion1024 <890700.ok@163.com>
2018-01-28 19:38:36 +0800

    Fix missing #includes in bootable/recovery.
    Signed-off-by: codelion1024 <890700.ok@163.com>

--- verifier_test.cpp
+++ verifier_test.cpp
@@ -14,13 +14,14 @@
  * limitations under the License.
  */
 
+#include <errno.h>
+#include <fcntl.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 
 #include "common.h"
 #include "verifier.h"
